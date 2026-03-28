726c75d8263933e2d0e7b46ea6577d6901b4897f
Vinícius Santos <3izuna@gmail.com>
2026-03-28 01:34:21 -0300

    Adicionando mais icones

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,29 @@
 import { LiaUserSecretSolid } from "react-icons/lia";
+import { FaRegEye } from "react-icons/fa";
+import { FaEyeLowVision } from "react-icons/fa6";
+import { FaHistory } from "react-icons/fa";
+import styles from '../header/header.module.css' 
+import { useState } from "react";
 
 
 const Header = () => {
+
+    const [eyeState, setEyeState] = useState(true)
   return (
     <>
-      <header>
-        <div className="icon_user">
-            <LiaUserSecretSolid id='personal_icon' />
+      <header className={styles.headerAcc}>
+        <div className={styles.icon_user}>
+            <LiaUserSecretSolid color="lightgray" size={50}/>
+        </div>
+
+        <div onClick={() => setEyeState(!eyeState)} className={styles.icon_usage}>
+            {
+                eyeState
+                ? <FaRegEye className={styles.icons_hover} size={26} />
+                : <FaEyeLowVision className={styles.icons_hover} size={26}/>
+            }
+            
+            <FaHistory className={styles.icons_hover} size={26} />
         </div>
       </header>    
     </>
