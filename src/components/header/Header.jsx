5771940d961147efd827c500940e33ce2ef4003c
Vinícius Santos <3izuna@gmail.com>
2026-03-28 01:13:40 -0300

    Adicionando o icon[ no header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,13 @@
+import { LiaUserSecretSolid } from "react-icons/lia";
+
 
 const Header = () => {
   return (
     <>
       <header>
-        
+        <div className="icon_user">
+            <LiaUserSecretSolid id='personal_icon' />
+        </div>
       </header>    
     </>
   )
