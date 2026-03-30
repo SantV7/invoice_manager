260763d5824c7e23adf8a989dfa7073806f9ebb9
Vinícius Santos <3izuna@gmail.com>
2026-03-30 16:05:09 -0300

    Adicionando a área de valores

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import '../src/global.css'
 import Header from './components/header/Header'
+import MainValues from './components/main_content/MainValues'
 function App() {
   
   return (
     <>
     <Header />
-    
+    <MainValues />
     </>
   )
 }
---
+++ src/components/main_content/MainValues.jsx
@@ -0,0 +1,15 @@
+import styles from '../main_content/mainValues.module.css'
+const MainValues = () => {
+  return (
+     <>
+     <main>
+        <section className={styles.bank_balance}>
+
+        </section>
+
+     </main>
+     </>
+  )
+}
+
+export default MainValues
