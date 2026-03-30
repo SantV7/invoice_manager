1a6372908d340776c1d0d4b8c0d793ac7a3254f8
Vinícius Santos <3izuna@gmail.com>
2026-03-30 16:17:13 -0300

    Fazendo a nova area

--- src/components/main_content/MainValues.jsx
+++ src/components/main_content/MainValues.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import styles from '../main_content/mainValues.module.css'
+
+
 const MainValues = () => {
+    const [bankBalance, setBankBalance] = useState(0)
+
+
+
+
+
   return (
      <>
-     <main>
+     <main id={styles.main_value_area}>
         <section className={styles.bank_balance}>
-
+            <h3 className={styles.bank_balance_state}>Saldo: {bankBalance}</h3>
+            <button id={styles.add_bank_balance_value}>Adicionar saldo</button>
         </section>
 
      </main>
