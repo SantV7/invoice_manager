5478faa65cef31b441a5d437a5de34dbcd8ce16a
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-19 02:02:29 -0300

    More ....

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,20 @@ function App() {
   const [eyeState, setEyeState] = useState(true)
   const [showTimer, setShowTimer] = useState('')
 
-// Refaer o horário e a data (  vc foi burro e falhou várias vezes)
+// Refazer o horário e a data (vc foi burro e falhou várias vezes)
+
+useEffect(() => {
+
+  function updateTime() {
+    //  AGORA DEVO CRAIR A FUNÇÃO E SUAS FEATURES
+  }
+
+  const intervalTimer = setInterval(() => {
+    (updateTime)
+  }, 1000);
+
+  return () => clearInterval(intervalTimer)
+}, [])
 
 
   
