25d616318750f8bbdf86f960168839bdb72f66f8
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-19 14:39:49 -0300

    Updating timer

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,36 @@ import { useEffect, useState } from 'react'
 import './global.css'
 import Header from './components/header/Header'
 import MainValues from './components/main_content/MainValues'
+
 function App() {
 
   const [eyeState, setEyeState] = useState(true)
   const [showTimer, setShowTimer] = useState('')
+  const [dayInfo, setDayInfo] = useState('')
 
 
+
+  useEffect(() => {
+   const dayWeek = {
+    0: 'Domingo',
+    1: 'Segunda',
+    2: 'Terça',
+    3: 'Quarta',
+    4: 'Quinta',
+    5: 'Sexta',
+    6: 'Sábado',
+  }
+    const now = new Date()
+    const dayNow = dayWeek[now.getDay()]
+    setDayInfo(dayNow)
+  }, [])
 
 useEffect(() => {
   function updateTime() {
     const timeGetter = new Date()
     const hours = timeGetter.getHours()
     const minutes = timeGetter.getMinutes()
-    setShowTimer(`${hours}:${minutes}h`)
+    setShowTimer(`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}h`)
   }
 
   const intervalTimer = setInterval(() => {
@@ -23,6 +40,7 @@ useEffect(() => {
 
   return () => clearInterval(intervalTimer)
 }, [])
+
 
 
   
@@ -35,6 +53,7 @@ useEffect(() => {
     <footer>
       <section>
         <p>{showTimer}</p>
+        <div>{dayInfo}</div>
       </section>
       
     </footer>
