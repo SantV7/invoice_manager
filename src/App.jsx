3eccd1fcc368780bddcd9adad83a23e98ae844d6
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-19 01:50:37 -0300

    Criando...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './global.css'
 import Header from './components/header/Header'
 import MainValues from './components/main_content/MainValues'
 function App() {
 
   const [eyeState, setEyeState] = useState(true)
+  const [showTimer, setShowTimer] = useState(0)
 
-  const [timerCounter, setTimerCounter] = useState(0)
+  
+  const updatingTimer = () => {
+    useEffect(() => {
+      setInterval(() => {
+        const [timerHours, setTimerHours] = useState(0)
+        const [timerMinutes, setTimerMinutes] = useState(0)
+        const [timerSeconds, setTimerSeconds] = useState(0)
+
+        const timerGetter = new Date()
+        setTimerHours(timerGetter.getHours())
+        setTimerMinutes(timerGetter.getMinutes())
+        setTimerSeconds(timerGetter.getSeconds())
+
+        setShowTimer(`${timerHours.toString().padStart('0', 2)}:${timerMinutes.toString().padStart('0', 2)}:${timerSeconds.toString().padStart('0', 2)}`)
+      },1000)      
+    }, [])
+  }
+
+
   
   return (
     <>
@@ -16,7 +35,7 @@ function App() {
 
     <footer>
       <section>
-        <p>timeHour</p>
+        <p>{showTimer}</p>
       </section>
       
     </footer>
